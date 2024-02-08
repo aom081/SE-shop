e4bac363d6b6d66299680d4e72d98d175a8c4a03
aom081 <matemsom@gmail.com>
2024-02-08 16:33:40 +0700

    8-2-2567

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div>
         <Banner />
         <Categories/>
-        <SpecialProduct/>
+      <SpecialProduct />
         <Testimonials/>
         <OurServices/>
     </div>
